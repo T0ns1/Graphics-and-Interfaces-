20c14e5cb7be3d41bca59e4ec618c3122e3b7b9a
António Mendes <95042431+T0ns1@users.noreply.github.com>
2022-10-21 15:23:16 +0100

    update

--- labs/ex12/app.js
+++ labs/ex12/app.js
@@ -85,17 +85,12 @@ function setup(shaders)
     });
     document.getElementById("add_cube").addEventListener("click", function(index) {
         instances.push("cube");
-        const box = document.getElementById("object_instances");
-        let option = document.createElement("option");
-        option.text = "Cube " + instances.length-1;
-        option.id = instances.length-1;
-        box.add(option);
     });
     document.getElementById("add_sphere").addEventListener("click", function(index) {
         instances.push("sphere");
         const box = document.getElementById("object_instances");
         let option = document.createElement("option");
-        option.text = "Sphere " + instances.length-1;
+        option.text = "Sphere " + (instances.length-1);
         option.id = instances.length-1;
         box.add(option);
     });
