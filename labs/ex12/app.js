604c40b2d26fea4e6f345f67a0f57d58570b12b6
unknown <antonio.n.mendes@gmail.com>
2022-10-28 05:19:54 +0100

    Can now edit selected instances

--- labs/ex12/app.js
+++ labs/ex12/app.js
@@ -18,15 +18,15 @@ const edge = 2.0;
 let instances = [];
 
 // Global Variables for our transformations
-let px = 0.0;
-let py = 0.0;
-let pz = 0.0;
-let sx = 1.0;
-let sy = 1.0;
-let sz = 1.0;
-let rx = 1.0;
-let ry = 0.0;
-let rz = 0.0;
+let px = [];
+let py = [];
+let pz = [];
+let sx = [];
+let sy = [];
+let sz = [];
+let rx = [];
+let ry = [];
+let rz = [];
 
 
 function render(time)
@@ -37,19 +37,22 @@ function render(time)
 
     gl.useProgram(program);
     
-    let t = translate(px, py, pz);
-    let rZ = rotateZ(rz);
-    let rY = rotateY(ry);
-    let rX = rotateX(rx);
-    let s = scalem(sx, sy, sz);
-        
-    let mModel = mult(t, mult(rZ, mult(rY, mult(rX, s))));
-    
-    const uCtm = gl.getUniformLocation(program, "uCtm");
-    //gl.uniformMatrix4fv(uCtm, false, flatten(mult(mProjection, mult(mView, mat4()))));
-    gl.uniformMatrix4fv(uCtm, false, flatten(mult(mProjection, mult(mView, mModel))));
-    
     for (let i = 0; i < instances.length; i++) {
+        let t = translate(px[i], py[i], pz[i]);
+        let rZ = rotateZ(rz[i]);
+        let rY = rotateY(ry[i]);
+        let rX = rotateX(rx[i]);
+        let s = scalem(sx[i], sy[i], sz[i]);
+        
+        let mModel = mult(t, mult(rZ, mult(rY, mult(rX, s))));
+
+        const uCtm = gl.getUniformLocation(program, "uCtm");
+        const uColor = gl.getUniformLocation(program, "uColor");
+
+        gl.uniformMatrix4fv(uCtm, false, flatten(mult(mProjection, mult(mView, mModel))));
+        if (i == document.getElementById("object_instances").selectedIndex) gl.uniform3f(uColor, 1.0, 0.0, 0.0);
+        else gl.uniform3f(uColor, 1.0, 1.0, 1.0);
+
         switch(instances[i]) {
             case "cube":
                 CUBE.draw(gl, program, gl.LINES);
@@ -102,21 +105,59 @@ function setup(shaders)
     
         gl.viewport(0,0,canvas.width, canvas.height);
     });
-    document.getElementById("add_cube").addEventListener("click", function(index) {
+    document.getElementById("add_cube").addEventListener("click", function() {
         instances.push("cube");
+        
+        px.push(1.0);
+        py.push(1.0);
+        pz.push(1.0);
+        rx.push(1.0);
+        ry.push(0.0);
+        rz.push(0.0);
+        sx.push(1.0);
+        sy.push(1.0);
+        sz.push(1.0);
+
         const box = document.getElementById("object_instances");
+        
         let option = document.createElement("option");
         option.text = "Cube " + (instances.length-1);
         option.id = instances.length-1;
+        
         box.add(option);
     });
-    document.getElementById("add_sphere").addEventListener("click", function(index) {
+    document.getElementById("add_sphere").addEventListener("click", function() {
         instances.push("sphere");
+
+        px.push(1.0);
+        py.push(1.0);
+        pz.push(1.0);
+        rx.push(1.0);
+        ry.push(0.0);
+        rz.push(0.0);
+        sx.push(1.0);
+        sy.push(1.0);
+        sz.push(1.0);
+
         const box = document.getElementById("object_instances");
+
         let option = document.createElement("option");
         option.text = "Sphere " + (instances.length-1);
         option.id = instances.length-1;
+
         box.add(option);
+    });
+    document.getElementById("object_instances").addEventListener("change", function() {
+        const index = document.getElementById("object_instances").selectedIndex;
+        document.getElementById("px").value = px[index];
+        document.getElementById("py").value = py[index];
+        document.getElementById("pz").value = pz[index];
+        document.getElementById("rx").value = rx[index];
+        document.getElementById("ry").value = ry[index];
+        document.getElementById("rz").value = rz[index];
+        document.getElementById("sx").value = sx[index];
+        document.getElementById("sy").value = sy[index];
+        document.getElementById("sz").value = sz[index];
     });
     document.getElementById("remove").addEventListener("click", function() {
         const box = document.getElementById("object_instances");
@@ -128,6 +169,15 @@ function setup(shaders)
             document.getElementById(i).remove();
             if (i+1 != length) { 
                 instances.splice(i, 1, instances[i+1]);
+                px.splice(i, 1, px[i+1]);
+                py.splice(i, 1, py[i+1]);
+                pz.splice(i, 1, pz[i+1]);
+                rx.splice(i, 1, rx[i+1]);
+                ry.splice(i, 1, ry[i+1]);
+                rz.splice(i, 1, rz[i+1]);
+                sx.splice(i, 1, sx[i+1]);
+                sy.splice(i, 1, sy[i+1]);
+                sz.splice(i, 1, sz[i+1]);
 
                 let option = document.createElement("option");
                 switch(instances[i+1]) {
@@ -143,37 +193,65 @@ function setup(shaders)
                         break;
                 }
             }
-            else instances.pop();
+            else {
+                instances.pop();
+                px.pop();
+                py.pop();
+                pz.pop();
+                rx.pop();
+                ry.pop();
+                rz.pop();
+                sx.pop();
+                sy.pop();
+                sz.pop();
+            }
         }
         console.log(instances);
-
     });
     document.getElementById("px").addEventListener("change", function() {
-        px = document.getElementById("px").value;
+        const box = document.getElementById("object_instances");
+        const sIndex = box.selectedIndex;
+        px[sIndex] = document.getElementById("px").value;
     });
     document.getElementById("py").addEventListener("change", function() {
-        py = document.getElementById("py").value;
+        const box = document.getElementById("object_instances");
+        const sIndex = box.selectedIndex;
+        py[sIndex] = document.getElementById("py").value;
     });
     document.getElementById("pz").addEventListener("change", function() {
-        pz = document.getElementById("pz").value;
+        const box = document.getElementById("object_instances");
+        const sIndex = box.selectedIndex;
+        pz[sIndex] = document.getElementById("pz").value;
     });
     document.getElementById("rx").addEventListener("change", function() {
-        rx = document.getElementById("rx").value;
+        const box = document.getElementById("object_instances");
+        const sIndex = box.selectedIndex;
+        rx[sIndex] = document.getElementById("rx").value;
     });
     document.getElementById("ry").addEventListener("change", function() {
-        ry = document.getElementById("ry").value;
+        const box = document.getElementById("object_instances");
+        const sIndex = box.selectedIndex;
+        ry[sIndex] = document.getElementById("ry").value;
     });
     document.getElementById("rz").addEventListener("change", function() {
-        rz = document.getElementById("rz").value;
+        const box = document.getElementById("object_instances");
+        const sIndex = box.selectedIndex;
+        rz[sIndex] = document.getElementById("rz").value;
     });
     document.getElementById("sx").addEventListener("change", function() {
-        sx = document.getElementById("sx").value;
+        const box = document.getElementById("object_instances");
+        const sIndex = box.selectedIndex;
+        sx[sIndex] = document.getElementById("sx").value;
     });
     document.getElementById("sy").addEventListener("change", function() {
-        sy = document.getElementById("sy").value;
+        const box = document.getElementById("object_instances");
+        const sIndex = box.selectedIndex;
+        sy[sIndex] = document.getElementById("sy").value;
     });
     document.getElementById("sz").addEventListener("change", function() {
-        sz = document.getElementById("sz").value;
+        const box = document.getElementById("object_instances");
+        const sIndex = box.selectedIndex;
+        sz[sIndex] = document.getElementById("sz").value;
     });
 
 
