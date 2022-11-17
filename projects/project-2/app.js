2286021ac09f469e7b536c07c48cfbcbff294de5
unknown <antonio.n.mendes@gmail.com>
2022-11-17 08:36:27 +0000

    Code cleanup and different cameras added

--- projects/project-2/app.js
+++ projects/project-2/app.js
@@ -1,111 +1,167 @@
 import { loadShadersFromURLS, setupWebGL, buildProgramFromSources } from '../../libs/utils.js';
-import { mat4, vec3, flatten, lookAt, ortho, mult, cross, subtract, add, rotateZ } from '../../libs/MV.js';
-import {modelView, loadMatrix, multRotationY, multScale, pushMatrix, popMatrix, multTranslation, multRotationX, multRotationZ } from "../../libs/stack.js";
+import { vec3, flatten, lookAt, ortho } from '../../libs/MV.js';
+import { modelView, loadMatrix, pushMatrix, popMatrix, multTranslation, multRotationX, multRotationY, multRotationZ,  multScale } from "../../libs/stack.js";
+
 import * as CUBE from '../../libs/objects/cube.js';
 import * as SPHERE from '../../libs/objects/sphere.js';
 import * as CYLINDER from '../../libs/objects/cylinder.js';
 
-/** @type {WebGLRenderingContext} */
-let gl;
-
-let program;
-
-/** View and Projection matrices */
-let mView;
-let mProjection;
-
 const edge = 50;
 
-/** Theta and gamma (degrees) for our projections */
-let theta = 90;
-let gamma = 45;
-
-/** Helicopter animation parameters */
-let alpha = 0;
-let dAlpha = 0;
-let beta = 0;
-let delta = 0;
-let dDelta = 0;
-let height = 0;
-const MAX_HEIGHT = 40;
-let time = 0;
-const speed = 1/60;
-let engineAnimation = false;
-let movementAnimation = false;
-
-/** Helicopter objects */
-let engineStarted = false;
-
-let mode;
-let animation = true;
-
-function render()
+function setup(shaders)
 {
+    let canvas = document.getElementById('gl-canvas');
+    let aspect = canvas.width / canvas.height;
+
+    /** @type WebGL2RenderingContext */
+    let gl = setupWebGL(canvas);
+
+    let mode = gl.TRIANGLES;
+
+    let program = buildProgramFromSources(gl, shaders['shader.vert'], shaders['shader.frag']);
+
+    /** Theta and gamma (degrees) for our camera coordinates */
+    let theta = 90;
+    let gamma = 45;
+
+    const eye = vec3(3*Math.cos(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180),3*Math.sin(gamma*Math.PI/180),3*Math.sin(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180));
+    const at = vec3(0,0,0);
+    const up = vec3(0,1,0);
+
+    let mView = lookAt(eye, at, up);
+    let mProjection = ortho(-edge*aspect,edge*aspect, -edge, edge,-3*edge,3*edge);
+
+    let zoom = 1.0;
+
+    /** Helicopter animation parameters */
+    let alpha = 0;
+    let dAlpha = 0;
+    let beta = 0;
+    let delta = 0;
+    let dDelta = 0;
+    let height = 0;
+    const MAX_HEIGHT = 40;
+    let time = 0;
+    const speed = 1/60;
+    let engineAnimation = false;
+    let movementAnimation = false;
+    let engineStarted = false;
+    let animation = true;
+
+    resize_canvas();
+    window.addEventListener("resize", resize_canvas);
+
+    document.onkeydown = function(event) {
+        switch(event.key) {
+            case '1':
+                // Regular view
+                mView = lookAt(vec3(3*Math.cos(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180),3*Math.cos(gamma*Math.PI/180),3*Math.sin(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180)), vec3(0,0,0), vec3(0,1,0));
+                break;
+            case '2':
+                // Front view
+                mView = lookAt([0,0,1], [0,0.0,0], [0,1,0]);
+                break;
+            case '3':
+                // Top view
+                mView = lookAt([0,1,0],  [0,0,0], [0,0,-1]);
+                break;
+            case '4':
+                // Right view
+                mView = lookAt([1,0,0], [0,0,0], [0,1,0]);
+                break;
+            case "d":
+                theta--;
+                theta = Math.max(theta, 0);
+                mView = lookAt(vec3(3*Math.cos(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180),3*Math.cos(gamma*Math.PI/180),3*Math.sin(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180)), vec3(0,0,0), vec3(0,1,0));
+                break;
+            case "a":
+                theta++;
+                theta = Math.min(theta,359);
+                mView = lookAt(vec3(3*Math.cos(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180),3*Math.cos(gamma*Math.PI/180),3*Math.sin(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180)), vec3(0,0,0), vec3(0,1,0));
+                break;
+            case "w":
+                gamma--;
+                gamma = Math.max(gamma,0);
+                mView = lookAt(vec3(3*Math.cos(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180),3*Math.cos(gamma*Math.PI/180),3*Math.sin(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180)), vec3(0,0,0), vec3(0,1,0));
+                break;
+            case "s":
+                gamma++;
+                gamma = Math.min(gamma,359);
+                mView = lookAt(vec3(3*Math.cos(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180),3*Math.cos(gamma*Math.PI/180),3*Math.sin(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180)), vec3(0,0,0), vec3(0,1,0));
+                break;
+            case "m":
+                if (mode == gl.TRIANGLES) mode = gl.LINES;
+                else mode = gl.TRIANGLES;
+                break;
+            case "ArrowUp":
+                if (!engineStarted) engineAnimation = true;
+                else {
+                    height += 0.2;
+                    height = Math.min(MAX_HEIGHT, height);
+                }
+                break;
+            case "ArrowDown":
+                height -= 0.2;
+                height = Math.max(0, height);
+                break;
+            case "ArrowLeft":
+                movementAnimation = true;
+                break;
+            case '+':
+                zoom /= 1.1;
+                break;
+            case '-':
+                zoom *= 1.1;
+                break;
+        }
+
+    }
+
+    document.onkeyup = function(event) {
+        switch(event.key) {
+        case "ArrowLeft":
+            movementAnimation = false;
+            break;
+        }
+    }
+
+    gl.clearColor(0.1, 0.7, 0.7, 1.0);
+    gl.enable(gl.DEPTH_TEST);   // Enables Z-buffer depth test
+
+    CUBE.init(gl);
+    SPHERE.init(gl);
+    CYLINDER.init(gl);
 
     window.requestAnimationFrame(render);
 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    function resize_canvas(event)
+    {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
 
-    gl.useProgram(program);
+        aspect = canvas.width / canvas.height;
 
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, "mProjection"), false, flatten(mProjection));
-
-    loadMatrix(mView);
-
-    if (animation) {
-        
-        if (engineAnimation && !engineStarted) {
-            time += speed;
-            dAlpha = easeInExpo(time, 0, 15, 5);
-            if (dAlpha >= 15) {
-                engineStarted = true;
-                time = 0;
-            }
-        }
-        
-        if (movementAnimation && engineStarted) {
-            time += speed;
-            if (time >= 0.85) time = 0.85;
-            beta = easeInOutCubic(time, 0, 30, 0.85);
-            dDelta = easeInExpo(time, 0, 2, 0.85);
-        }
-        if (!movementAnimation && engineStarted) {
-            console.log("hello");
-            time -= speed;
-            if (time <= 0) time = 0;
-            beta = easeInExpo(time, 0, 30, 0.85);
-            dDelta = easeInExpo(time, 0, 2, 0.85);
-            console.log(beta);
-            console.log(dDelta);
-        }
-
-        alpha += dAlpha;
-        delta += dDelta;
+        gl.viewport(0,0,canvas.width, canvas.height);
+        mProjection = ortho(-aspect*edge*zoom,aspect*edge*zoom, -zoom*edge, zoom*edge,-3*edge,3*edge);
     }
 
-    floor();
-    pushMatrix();
-        multRotationY(-delta);
-        multTranslation([0.0,height,50.0]);
-        multRotationZ(beta);
-        Helicopter();
-    popMatrix();
+    function uploadProjection()
+    {
+        uploadMatrix("mProjection", mProjection);
+    }
 
     function uploadModelView()
     {
-        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mModelView"), false, flatten(modelView()));
+        uploadMatrix("mModelView", modelView());
+    }
+
+    function uploadMatrix(name, m) {
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, name), false, flatten(m));
     }
 
     function easeInExpo (t, b, c, d) {
         if (t >= d) t = d;
         return (t == 0) ? b : c * Math.pow(2, 10 * (t / d - 1)) + b;
-    }
-
-    function easeInOutExpo (t, b, c, d) {
-        if (t == 0) return b;
-        if (t == d) return b + c;
-        if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
-        return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
     }
 
     function easeInOutCubic (t, b, c, d) {
@@ -119,7 +175,7 @@ function render()
         gl.uniform3fv(uColor, vec3(0.1,0.1,0.1)); //grey
 
         pushMatrix();
-            multScale([150,1,150]);
+            multScale([110,1,110]);
             multTranslation([0.0,0.5,0.0]);
             uploadModelView();
             CUBE.draw(gl, program, mode);
@@ -132,9 +188,9 @@ function render()
         gl.uniform3fv(uColor, vec3(1.0, 0.0, 0.0)); // red
         
         // Scale helicopter
-        multScale([7.2,10,7.2]);
+        multScale([7.2,7.2,7.2]);
         // level with ground
-        multTranslation([0.0,0.37,0.0]);
+        multTranslation([0.0,0.42,0.0]);
 
         pushMatrix();
             multScale([0.8, 0.3, 0.3]);
@@ -260,104 +316,64 @@ function render()
         
     }
 
-}
+    function render()
+    {
+        window.requestAnimationFrame(render);
 
-
-
-function setup(shaders)
-{
-    let canvas = document.getElementById('gl-canvas');
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    let aspect = canvas.width / canvas.height;
-
-    gl = setupWebGL(canvas);
-    program = buildProgramFromSources(gl, shaders['shader.vert'], shaders['shader.frag']);
-
-    mode = gl.TRIANGLES;
-
-    gl.clearColor(0.1, 0.7, 0.7, 1.0);
-    gl.viewport(0,0,canvas.width, canvas.height);
-
-    const eye = vec3(3*Math.cos(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180),3*Math.sin(gamma*Math.PI/180),3*Math.sin(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180));
-    const at = vec3(0,0,0);
-    const up = vec3(0,1,0);
-    mView = lookAt(eye, at, up);
-    mProjection = ortho(-edge*aspect,edge*aspect, -edge, edge,-3*edge,3*edge);
-
-    CUBE.init(gl);
-    SPHERE.init(gl);
-    CYLINDER.init(gl);
-
-    gl.enable(gl.DEPTH_TEST);   // Enables Z-buffer depth test
-
-    window.requestAnimationFrame(render);
-
-    window.addEventListener("resize", function() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-
-        aspect = canvas.width / canvas.height;
-
-        gl.viewport(0,0,canvas.width, canvas.height);
-        mProjection = ortho(-edge*aspect,edge*aspect, -edge, edge,-3*edge,3*edge);
-    });
-
-    window.addEventListener("keydown", function(event) {
-        console.log(event.key);
-        switch(event.key) {
-            case "d":
-                theta++;
-                if (theta >= 360) theta = theta - 360;
-                console.log(theta);
-                break;
-            case "a":
-                theta--;
-                if (theta <= 0) theta = theta + 360;
-                console.log(theta);
-                break;
-            case "w":
-                gamma++;
-                if (gamma >= 360) gamma = gamma - 360;
-                console.log(gamma);
-                break;
-            case "s":
-                gamma--;
-                if (gamma <= 0) gamma = gamma + 360;
-                console.log(gamma);
-                break;
-            case "m":
-                if (mode == gl.TRIANGLES) mode = gl.LINES;
-                else mode = gl.TRIANGLES;
-                break;
-            case "ArrowUp":
-                if (height == 0 && !engineStarted) engineAnimation = true;
-                if (engineStarted && height < MAX_HEIGHT) height+=0.1;
-                break;
-            case "ArrowDown":
-                if (height > 0) height-=0.1;
-                break;
-            case "ArrowLeft":
-                movementAnimation = true;
-                break;
-        }
-        window.addEventListener("keyup", function(event) {
-            console.log(event.key);
-            switch(event.key) {
-            case "ArrowLeft":
-                movementAnimation = false;
-                break;
-            }
-        })
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         
-        mView = lookAt(vec3(3*Math.cos(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180),3*Math.cos(gamma*Math.PI/180),3*Math.sin(theta*Math.PI/180)*Math.sin(gamma*Math.PI/180)), vec3(0,0,0), vec3(0,1,0));
+        gl.useProgram(program);
+        
+        // Send the mProjection matrix to the GLSL program
+        mProjection = ortho(-aspect*edge*zoom,aspect*edge*zoom, -zoom*edge, zoom*edge,-3*edge,3*edge);
+        uploadProjection(mProjection);
 
-    });
+        // Load the ModelView matrix with the Worl to Camera (View) matrix
+        loadMatrix(mView);
+
+        if (animation) {
+            if (engineAnimation && !engineStarted) {
+                time += speed;
+                dAlpha = easeInExpo(time, 0, 15, 5);
+                if (dAlpha >= 15) {
+                    engineStarted = true;
+                    time = 0;
+                }
+            }
+
+            if (engineStarted) {
+                if (movementAnimation) {
+                    time += speed;
+                    time = Math.min(time,0.85);
+
+                    beta = easeInOutCubic(time, 0, 30, 0.85);
+                    dDelta = easeInExpo(time, 0, 2, 0.85);
+                }
+                else {
+                    time -= speed;
+                    time = Math.max(time, 0);
+
+                    beta = easeInExpo(time, 0, 30, 0.85);
+                    dDelta = easeInExpo(time, 0, 2, 0.85);
+                }
+            }
+
+            alpha += dAlpha;
+            delta += dDelta;
+        }
+
+        floor();
+        pushMatrix();
+            multRotationY(-delta);
+            multTranslation([0.0,height,50.0]);
+            multTranslation([-2.5,0.0,0.0]);
+            multRotationZ(beta);
+            multTranslation([2.5,0.0,0.0]);
+            Helicopter();
+        popMatrix();
+    }
 
 }
 
 const shaderUrls = ['shader.vert', 'shader.frag'];
-
 loadShadersFromURLS(shaderUrls).then(shaders=>setup(shaders));
